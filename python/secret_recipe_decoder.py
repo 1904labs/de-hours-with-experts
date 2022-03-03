45c01ec4ea8a245b16243db51d6d983225099783
janelore <janettebarber88@gmail.com>
2022-03-03 07:25:26 -0600

    Update secret_recipe_decoder.py
    completed code

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -3,6 +3,8 @@ import pydoc
 import sys
 import os
 
+f = open('secret_recipe.txt','r')
+print(f)
 # Caesar encoding, for use with decoding below
 ENCODING = {
     'y': 'a',
@@ -50,20 +52,39 @@ class Ingredient():
         self.amount = amount
         self.description = description
 
-
+    def __repr__(self) -> str: 
+        return f"{self.amount} {self.description}"
+    
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
-
+    result = ''
+    for character in str:
+        if character in ENCODING:
+            result = result + ENCODING[character]
+        else:
+            result = result + character
+    return result
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
-
+    split_line = line.split('#')
+    amount = decode_string(split_line[0])
+    description = decode_string(split_line[1])
+    return Ingredient(amount,description)
+       
+print(str(decode_ingredient('8 vgl#hgiikf')))
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
-
+    with open('secret_recipe.txt') as f:
+        contents = f.readlines()
+        print(contents)
+    
+        for item in contents:
+            print(str(decode_ingredient(item)))
+            
 if __name__ == "__main__":
     main()
