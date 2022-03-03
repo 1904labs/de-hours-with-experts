27f531bdf34b527b16f28738b1fca123318ba185
Richard WIlliams <riwi2802@colorado.edu>
2022-03-03 11:55:15 -0500

    completed application in python

--- java/src/main/java/com/labs1904/SecretRecipeDecoder.java
+++ java/src/main/java/com/labs1904/SecretRecipeDecoder.java
@@ -3,6 +3,9 @@ package com.labs1904;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import javax.sound.sampled.AudioFormat.Encoding;
 
 public class SecretRecipeDecoder {
     private static Map<String, String> ENCODING = new HashMap<String, String>() {
@@ -51,9 +54,64 @@ public class SecretRecipeDecoder {
      * @param str
      * @return
      */
+
+    private static Map<String, String> DECODING = new HashMap<String, String>() {
+        {
+            put("a", "y");
+            put("h", "b");
+            put("c", "v");
+            put("d", "x");
+            put("e", "k");
+            put("f", "p");
+            put("g", "z");
+            put("h", "s");
+            put("i", "a");
+            put("j", "b");
+            put("k", "e");
+            put("l", "w");
+            put("m", "u");
+            put("n", "q");
+            put("o", "n");
+            put("p", "l");
+            put("q", "m");
+            put("r", "f");
+            put("s", "o");
+            put("t", "i");
+            put("u", "g");
+            put("v", "j");
+            put("w", "t");
+            put("x", "d");
+            put("y", "r");
+            put("z", "c");
+            put("0", "3");
+            put("1", "8");
+            put("2", "4");
+            put("3", "0");
+            put("4", "2");
+            put("5", "7");
+            put("6", "5");
+            put("7", "9");
+            put("8", "1");
+            put("9", "6");
+        }
+    };
+
     public static String decodeString(String str) {
+
+        char[] encodedCharArray = str.toCharArray();
+
+        char[] decodedCharArray;
+        
+
+        for (int i = 0; i < str.length(); i++) {
+
+            decodedCharArray[i] = ENCODING.get(Character.toString(encodedCharArray[i]));
+
+
+        }
+
         // TODO: implement me
-        return "";
+        return decodedString;
     }
 
     /**
@@ -68,5 +126,9 @@ public class SecretRecipeDecoder {
 
     public static void main(String[] args) {
         // TODO: implement me
+
+        System.out.println(decodeString("hgiikf"));
+
+
     }
 }
