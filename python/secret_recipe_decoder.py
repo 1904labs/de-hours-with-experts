64a5ba13375a242737c4abde6d21f2d1604f7283
Richard WIlliams <riwi2802@colorado.edu>
2022-03-03 11:47:31 -0500

    completed application

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+from ast import AsyncFunctionDef
+from base64 import decode
+from fileinput import close
 import pydoc
+import string
 import sys
 import os
 
@@ -40,7 +44,12 @@ ENCODING = {
     '5': '6',
     '9': '7',
     '1': '8',
-    '6': '9'
+    '6': '9',
+    ' ': ' ',
+    ',': ',',
+    '\n':'\n',
+    '/':'/',
+    '-':'-'
  }
 
 """An ingredient has an amount and a description.
@@ -55,15 +64,52 @@ def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
 
+    list = []
+
+    for i in range(len(str)):
+        list.append(ENCODING[str[i]]) 
+
+    decodedString =''.join(list)
+    return decodedString
+
+print(decode_string("hgiikf"))
+
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
 
+    ## i added a blank space to the ditionary to account for spacing to make things easier 
+
+    entire_line =line.split("#")
+    amount_encoded = entire_line[0]
+    description_encoded = entire_line[1]
+    i1 = Ingredient(decode_string(amount_encoded), decode_string(description_encoded))
+    return decode_string(amount_encoded) + " " + decode_string(description_encoded)
+
+
+print(decode_ingredient("8 vgl#hgiikf"))
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+
+    encoded_recipe = open("./secret_recipe.txt", "r")
+    # decoded_recipe = open("./decoded_recipe.txt", "x")
+    temp_text = ""
+
+    encoded_recipe.readline()
+
+    file = open("decoded_recipe.txt", "w")
+    
+    for x in encoded_recipe:
+        file.write(decode_ingredient(x))
+
+    file.close()
+
+        
+
+
 
 if __name__ == "__main__":
     main()
