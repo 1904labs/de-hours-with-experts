b6857235dd4c56b8a37c612950ee7cde0d0aeed4
Jasmine S <49571750+jksdk4@users.noreply.github.com>
2022-03-03 17:11:17 -0600

    got the getValue and getKey backwards

--- java/src/main/java/com/labs1904/SecretRecipeDecoder.java
+++ java/src/main/java/com/labs1904/SecretRecipeDecoder.java
@@ -56,8 +56,8 @@ public class SecretRecipeDecoder {
         String[] decodedArr = new String[str.length()];
         for (int i = 0; i < str.length(); i++) {
             for (Map.Entry<String, String> code : ENCODING.entrySet()) {
-                if (code.getKey().equals(Character.toString(str.charAt(i)))) {
-                    decodedArr[i] = code.getValue();
+                if (code.getValue().equals(Character.toString(str.charAt(i)))) {
+                    decodedArr[i] = code.getKey();
                 }
             }
         }
@@ -77,6 +77,6 @@ public class SecretRecipeDecoder {
 
     public static void main(String[] args) {
         // TODO: implement me
-        System.out.println(decodeString("hgiikf"));
+        System.out.println(decodeString("abc"));
     }
 }
