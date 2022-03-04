f9a1ed9589d57a18778c5f6760fc17f6e66e6781
David Dong <daviddong201612@gmail.com>
2022-03-04 08:35:51 -0600

    update challenge three, but there is an issue about file path.

--- java/src/main/java/com/labs1904/SecretRecipeDecoder.java
+++ java/src/main/java/com/labs1904/SecretRecipeDecoder.java
@@ -48,25 +48,96 @@ public class SecretRecipeDecoder {
 
     /**
      * Given a string named str, use the Caesar encoding above to return the decoded string.
+     *
      * @param str
      * @return
      */
     public static String decodeString(String str) {
         // TODO: implement me
-        return "";
+        List<String> decodeStrList = new ArrayList<>();
+
+        for (char cha : str.toCharArray()) {
+            String chaStr = Character.toString(cha);
+            if (ENCODING.containsKey(chaStr)) {
+                decodeStrList.add(ENCODING.get(chaStr));
+            } else {
+                decodeStrList.add(chaStr);
+            }
+        }
+        String decodeStr = String.join("", decodeStrList);
+
+        return decodeStr;
+
     }
 
     /**
      * Given an ingredient, decode the amount and description, and return a new Ingredient
+     *
      * @param line
      * @return
      */
     public static Ingredient decodeIngredient(String line) {
         // TODO: implement me
-        return null;
+        List<String> decodeStrListAmount = new ArrayList<>();
+        List<String> decodeStrListDescription = new ArrayList<>();
+
+        for (char cha : line.toCharArray()) {
+            String chaStr = Character.toString(cha);
+
+            if (chaStr != "#") {
+                if (ENCODING.containsKey(chaStr)) {
+                    decodeStrListAmount.add(ENCODING.get(chaStr));
+                } else {
+                    decodeStrListAmount.add(chaStr);
+                }
+            } else {
+                if (ENCODING.containsKey(chaStr)) {
+                    decodeStrListDescription.add(ENCODING.get(chaStr));
+                } else {
+                    decodeStrListDescription.add(chaStr);
+                }
+            }
+        }
+        String amount = String.join("", decodeStrListAmount);
+        String description = String.join("", decodeStrListDescription);
+
+        return new Ingredient(amount, description);
     }
 
     public static void main(String[] args) {
         // TODO: implement me
+        System.out.println("Challenge #1");
+        String str1 = "hgiikf";
+        System.out.println(decodeString(str1));
+
+        System.out.println("Challenge #2");
+        String str2 = "8 vgl#hgiikf";
+        System.out.println(decodeString(str2));
+
+        System.out.println("Challenge #3");
+        // File path is passed as parameter
+        File file = new File(
+                "C:\\dtg\\jobHunting\\20220302for1904labs\\de-hours-with-experts\\java\\src\\main\\resources\\secret_recipe.txt}");
+
+        /*File file = new File(
+                "C:\\Users\\16366\\Desktop\\secret_recipe.txt");
+
+        */
+
+        // Creating an object of BufferedReader class
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
+
+        // Declaring a string variable
+        String st;
+        List<String> decodeStr111 = new ArrayList<>();
+        // Condition holds true till
+        // there is character in a string
+        while ((st = br.readLine()) != null) {
+            System.out.println(decodeIngredient(st).getAmount());
+            System.out.println(decodeIngredient(st).getDescription());
+        }
+
+
     }
 }
