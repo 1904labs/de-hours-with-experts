7b94c6836728f7c92d79bbca22895df0dbb21379
Jonas Malave Campos <jonasmalavec@gmail.com>
2022-03-04 19:04:17 -0600

    3rd Challenge Solution V2 - Jonas Malave Campos

--- python/test_secret_recipe_decoder.py
+++ python/test_secret_recipe_decoder.py
@@ -1,9 +1,10 @@
 import pytest
 from secret_recipe_decoder import decode_ingredient, encode_string, decode_string, Ingredient
 
+#Modified this function seems instructions seem to point to encode rather than decode
 def test_encode_string_can_decode():
     assert(encode_string("abc") == "yhv")
-
+#Added this function for the purposes of decoding in case this was a requirement as well
 def test_decode_string_can_decode():
     assert(decode_string("abc") == "ijz")
 
