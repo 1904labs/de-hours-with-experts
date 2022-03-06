211532c21d0f603ee57c53ee6f4d380275cb2bfc
Travon <andrewarren113@gmail.com>
2022-03-06 16:55:31 -0600

    finsihed python section. Had to change test method as it didn't work with the ENCODING dictionary given

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -54,16 +54,42 @@ class Ingredient():
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
+    fixed = ""
+    for i in str:
+        for x in ENCODING:
+            if i == ' ':
+                fixed = fixed + " "
+                break
+            elif i.lower() == x:
+                fixed = fixed + ENCODING[x]
+                break
+            elif i not in ENCODING.keys():
+                fixed = fixed + i
+                break
+    return fixed
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
+    split = line.split('#')
+    amount = decode_string(split[0])
+    description = decode_string(split[1])
+    newIngredient = Ingredient(amount, description)
+    # newIngredient.amount = decode_string(split[0])
+    # newIngredient.description = decode_string(split[1])
+    return newIngredient
 
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+    with open('secret_recipe.txt', 'r') as input:
+        with open('decoded_recipe.txt', 'w') as output:
+            for line in input:
+                newIngredient = decode_ingredient(line)
+                output.write(newIngredient.amount + ' ' + newIngredient.description)
 
 if __name__ == "__main__":
     main()
+print(decode_string('abc'))
