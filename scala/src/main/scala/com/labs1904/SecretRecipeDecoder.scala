82e8fe62735af446594dea067d097fb0dfa5dc05
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2022-03-06 17:33:09 -0600

    finishes decoding function

--- scala/src/main/scala/com/labs1904/SecretRecipeDecoder.scala
+++ scala/src/main/scala/com/labs1904/SecretRecipeDecoder.scala
@@ -1,4 +1,7 @@
-package com.labs1904
+// package com.labs1904
+
+import java.io.PrintWriter
+import scala.io.Source
 
 import scala.collection.immutable.HashMap
 
@@ -54,14 +57,62 @@ object SecretRecipeDecoder {
    * @param str A caesar-encoded string.
    * @return
    */
-  def decodeString(str: String): String  = ???
+  def decodeString(str: String): String  = {
+    
+    var decoded: String = ""
+    
+    for (i <- str) {
+      // i is a char we must convert to string
+      val j = i.toString
+      decoded += ENCODING(j)
+      // println(ENCODING(j))
+    }
+    println("Decoded string : " + decoded)
+    // Last line returns
+    decoded
+  }
 
   /**
    * Given an ingredient, decode the amount and description, and return a new Ingredient
    * @param line An encoded ingredient.
    * @return
    */
-  def decodeIngredient(line: String): Ingredient = ???
+  def decodeIngredient(line: String): Ingredient = {
+    // Ingredient is a quantity # description
+
+    var quantity: String = ""
+    var description: String = ""
+    // While we have not reached the '#' push to quantity
+    var k = 0
+    var indexOfHash: Int = line.indexOf('#')
+    // println("Index of hash: " + indexOfHash)
+    while (k < line.length) {
+      // println("K: " + k)
+      var char = line(k).toString
+      if (k < indexOfHash) {
+        // println("char " + char)
+        if (ENCODING.contains(char)) {
+          quantity += ENCODING(char)
+        } else {
+          quantity += char
+        }
+      } else if (k > indexOfHash) {
+        // var char = line(k).toString
+        // println("char " + char)
+        if (ENCODING.contains(char)) {
+          description += ENCODING(char)
+        } else {
+          description += char
+        }
+      }
+      k += 1
+    }
+    // println("Decoded quantity: " + quantity)
+    // println("Decoded description: " + description)
+    // println("Decoded ingredient: " + decodedIngredient)
+    // decodedIngredient
+    new Ingredient(quantity, description)
+  }
 
   /**
    * A program that decodes a secret recipe
@@ -69,5 +120,16 @@ object SecretRecipeDecoder {
    */
   def main(args: Array[String]): Unit = {
     // TODO: implement me
+    val recipe = Source.fromFile("/Users/eyeballkid/Documents/Dev/hours-with-experts/de-hours-with-experts/scala/src/main/resources/secret_recipe.txt", "UTF-8")
+    val lines = recipe.getLines
+    val writer = new PrintWriter("decoded_recipe.txt")
+    for (line <- lines) {
+      var decodedIngredient = decodeIngredient(line)
+      var decodedLine = decodedIngredient.amount + " " + decodedIngredient.description
+      println(decodedLine)
+      writer.println(decodedLine)
+    }
+    writer.close()
+    // println(new java.io.File("./").getCanonicalPath)
   }
 }
