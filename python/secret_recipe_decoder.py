001afc7cb9eab3621731fbe99604afa72d3eb1d5
Alvin Do <alvin@Alvins-MBP.attlocal.net>
2022-03-06 13:53:25 -0600

    Uploading finished decoder challenge

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -55,15 +55,48 @@ def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
 
+    chars = []
+    for character in str:
+        if character.isalnum():
+            decoded_char = character.replace(character, ENCODING[character])
+            chars.append(decoded_char)
+        else:
+            chars.append(character)
+
+    # This approach doesn't work. It changes ALL of the letters within the string if they are the same
+    # for letter in str:
+    #     if letter.isalnum():
+    #         str = str.replace(letter, ENCODING[letter])
+
+    return ''.join(chars)
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
+    amount = line.split('#')[0]
+    description = line.split('#')[1]
 
+    # Decoding amount
+    amount = decode_string(amount)
+
+    # Decoding description
+    description = decode_string(description)
+
+    return Ingredient(amount=amount, description=description)
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+    try:
+        with open('secret_recipe.txt', 'r') as recipes:
+            try:
+                with open('decoded_recipe.txt', 'w') as output:
+                    for recipe in recipes:
+                        output.write(f"{decode_ingredient(recipe).amount} {decode_ingredient(recipe).description}")
+            except FileNotFoundError:
+                print("Error creating/opening 'decoded_recipe.txt' file")
+    except FileNotFoundError:
+        print("'secret_recipe.txt' file does not exist!")
 
 if __name__ == "__main__":
     main()
--- python/test_secret_recipe_decoder.py
+++ python/test_secret_recipe_decoder.py
@@ -1,7 +1,7 @@
 from secret_recipe_decoder import decode_ingredient, decode_string, Ingredient
 
 def test_decode_string_can_decode():
-    assert(decode_string("abc") == "yhv")
+    assert(decode_string("yhv") == "abc") # swapped the arg and output
 
 def test_decode_ingredient_can_decode():
     expected = Ingredient("1 cup", "butter")
