8a169a6b79a367a4709b7e0fa961e210bfb53c6e
ouima2021 <ouima2014@gmail.com>
2022-03-05 21:41:45 -0600

    Init Commit; completed decodeString, decodeIngredient functions.

--- java/src/main/java/com/labs1904/SecretRecipeDecoder.java
+++ java/src/main/java/com/labs1904/SecretRecipeDecoder.java
@@ -4,6 +4,9 @@ package com.labs1904;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
+
 public class SecretRecipeDecoder {
     private static Map<String, String> ENCODING = new HashMap<String, String>() {
         {
@@ -43,6 +46,7 @@ public class SecretRecipeDecoder {
             put("9", "7");
             put("1", "8");
             put("6", "9");
+            put(" ", " ");
         }
     };
 
@@ -53,7 +57,19 @@ public class SecretRecipeDecoder {
      */
     public static String decodeString(String str) {
         // TODO: implement me
-        return "";
+        String letter;
+        String finalWord = "";
+        for (int i = 0; i < str.length(); i++) {
+            letter = (String.valueOf(str.charAt(i)));
+
+        for (Map.Entry entry : ENCODING.entrySet()) {
+            if (entry.getValue().equals(letter)) {
+                finalWord += entry.getKey();
+            }
+        }
+    }
+
+        return finalWord;
     }
 
     /**
@@ -63,10 +79,85 @@ public class SecretRecipeDecoder {
      */
     public static Ingredient decodeIngredient(String line) {
         // TODO: implement me
-        return null;
+        String amountInRecipe = "";
+        String measurement = "";
+        String intermediateMeasurement = "";
+        String ingredientInRecipe = "";
+        String ingredient = "";
+        String intermediateIngredient = "";
+        String[] breakLine = line.split("[#]");
+
+        for (int i = 0; i < breakLine.length; i++) {
+            measurement = breakLine[0];
+            ingredient = breakLine[1];
+        }
+
+            for (int k = 0; k < breakLine[0].length(); k++) {
+                intermediateMeasurement = String.valueOf(measurement.charAt(k));
+                 System.out.println(intermediateMeasurement);
+
+                for (Map.Entry recipe2 : ENCODING.entrySet()){
+                    if (recipe2.getKey().equals(intermediateMeasurement)) {
+                        amountInRecipe += recipe2.getValue();
+                        // System.out.println(amountInRecipe);
+                    }
+                }
+
+            }
+
+            for (int j = 0; j < breakLine[1].length(); j++) {
+                intermediateIngredient = String.valueOf(ingredient.charAt(j));
+
+            for (Map.Entry recipe : ENCODING.entrySet()) {
+                if (recipe.getKey().equals(intermediateIngredient)) {
+                    ingredientInRecipe += recipe.getValue();
+                  //  System.out.println(ingredientInRecipe);
+                }
+            }
+        }
+
+
+            return new Ingredient(amountInRecipe, ingredientInRecipe);
     }
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
         // TODO: implement me
+
     }
 }
+
+
+
+
+ /*   char[] charInString = line.toCharArray();
+
+        for (char ch : charInString){
+           // System.out.println(ch);
+            for (Map.Entry recipe3 : ENCODING.entrySet()){
+                if (recipe3.getKey().equals(String.valueOf(ch))){
+                    intermediate += recipe3.getValue();
+                    System.out.println(intermediate);
+
+                }
+            }
+        }*/
+
+/* for (int i = 0; i < line.length(); i++) {
+           // System.out.println(breakLine[i].charAt(i));
+           // System.out.println(line.charAt(i));
+            string = (String.valueOf(line.charAt(i)));
+            for (Map.Entry recipe : ENCODING.entrySet()) {
+                if (recipe.getKey().equals(string)) {
+                   intermediate += recipe.getValue();
+                    System.out.println(intermediate);
+                }
+
+            }
+        }*/
