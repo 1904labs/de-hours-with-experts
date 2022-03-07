092e0441e75c7e105bc6e8dbcc24e4ed9a33fc8f
sudsubra <sudsubra@gmail.com>
2022-03-06 18:44:22 -0600

    sudsubra Challenge Three code submission

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -54,16 +54,41 @@ class Ingredient():
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
+    decoded_string= ""
+    for char in str.lower():
+        if char in ENCODING.keys():
+            d_letter = ENCODING[char]
+        decoded_string += d_letter
+            
+    return decoded_string
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
+    Ingredient = ''
+    for char in line.lower():
+        if char in ENCODING.keys():
+            new_char = ENCODING[char]
+        elif char == '#':
+            new_char = ', '    
+        else:
+            new_char = ' '
+        Ingredient += new_char
+    return Ingredient
 
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+    file = open('secret_recipe.txt','r')
+    lines = file.readline()
+
+    with open ('decoded_recipe.txt','w') as f:
+        for line in lines:
+            new_line = decode_ingredient(line)
+            f.write(new_line)
+            f.write('\n')  
 
 if __name__ == "__main__":
     main()
