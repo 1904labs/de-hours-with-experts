24d2e2699b8debf1acca88690f96bbf7611d7bb1
Broc Hammett <bxrayhamm@gmail.com>
2022-03-06 18:18:25 -0600

    complete Challenge Three

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -54,16 +54,43 @@ class Ingredient():
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
-
+    result = ""
+    char = 1
+    last_char = str[0]
+    
+    for i in range(1, len(str)):
+        if last_char == str[i]:
+            char += 1
+        else:
+            result += str(char) + last_char
+            char = 0
+            last_char = str[i]
+            char += 1
+    result += str(char) + last_char
+    return result
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
-
+    i = 0
+    with open("secret_recipe.txt", "r") as file:
+        for word in file:
+            i = i + 1
+            if line in word:
+                print(i)
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+    file1 = open("secret_recipe.txt", "r")
+    secret_recipe = file1.read()
+
+    with open("decoded_recipet.txt", "a+") as secret_recipe:
+        secret_recipe.seek(0)
+        data = secret_recipe.read()
+        if len(data) > 0:
+            secret_recipe.write("\n")
+        secret_recipe.write(decode_string, decode_ingredient)
 
 if __name__ == "__main__":
     main()
