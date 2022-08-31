39db176945ab8696b0cf2c70d5ff2c684d803b61
Carolina Scalzo <carolina.scalzo@slalom.com>
2022-08-30 20:45:27 -0500

    New file to show work

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -51,7 +51,7 @@ class Ingredient():
         self.description = description
 
 
-def decode_string(str): #completed#
+def decode_string(str): #completed
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
     
@@ -99,16 +99,70 @@ def decode_string(str): #completed#
     for letter in str:
         print(ENCODING[letter], end = "")
 
-decode_string("vsnllkx")
+decode_string("hgiikf")
 
-def decode_ingredient(line):
-    """Given an ingredient, decode the amount and description, and return a new Ingredient"""
-    # TODO: implement me
+# def decode_ingredient(line):
+#     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
+#     # TODO: implement me
+#     line = ""
 
+#     message = ""
 
-def main():
-    """A program that decodes a secret recipe"""
-    # TODO: implement me
+#     ENCODING = {
+#     'y': 'a',
+#     'h': 'b',
+#     'v': 'c',
+#     'x': 'd',
+#     'k': 'e',
+#     'p': 'f',
+#     'z': 'g',
+#     's': 'h',
+#     'a': 'i',
+#     'b': 'j',
+#     'e': 'k',
+#     'w': 'l',
+#     'u': 'm',
+#     'q': 'n',
+#     'n': 'o',
+#     'l': 'p',
+#     'm': 'q',
+#     'f': 'r',
+#     'o': 's',
+#     'i': 't',
+#     'g': 'u',
+#     'j': 'v',
+#     't': 'w',
+#     'd': 'x',
+#     'r': 'y',
+#     'c': 'z',
+#     '3': '0',
+#     '8': '1',
+#     '4': '2',
+#     '0': '3',
+#     '2': '4',
+#     '7': '5',
+#     '5': '6',
+#     '9': '7',
+#     '1': '8',
+#     '6': '9'
+#  }
+    
+# #     def amount():
+# #         for letter in line: 
+# #             if letter in ENCODING[letter]:
+# #                 message += ENCODING[letter]
+# #             else: 
+# #                 message += line[i]
+# #         print(message, end = '  ')
+# #         return amount 
+    
+# #     print(amount("8 vgl"))
 
-if __name__ == "__main__":
-    main()
+# # decode_ingredient("8 vgl")
+
+# def main():
+# #     """A program that decodes a secret recipe"""
+# #     # TODO: implement me
+
+#     if __name__ == "__main__":
+#     main()
