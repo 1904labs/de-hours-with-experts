670254286f0529979db40333ead06cab10857832
Carolina Scalzo <carolina.scalzo@slalom.com>
2022-09-01 10:00:54 -0500

    Final solution

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -49,6 +49,63 @@ nonsense_recipe = open("/Users/carolina.scalzo/de-hours-with-experts/python/secr
 #testing final solution below with different recipe
 # half_recipe = open("/Users/carolina.scalzo/de-hours-with-experts/python/halfrecipe.txt")
 
+#********* PART ONE *************#
+
+def decode_string(str): #completed
+    """Given a string named str, use the Caesar encoding above to return the decoded string."""
+    # TODO: implement me
+    
+    ENCODING = {
+    'y': 'a',
+    'h': 'b',
+    'v': 'c',
+    'x': 'd',
+    'k': 'e',
+    'p': 'f',
+    'z': 'g',
+    's': 'h',
+    'a': 'i',
+    'b': 'j',
+    'e': 'k',
+    'w': 'l',
+    'u': 'm',
+    'q': 'n',
+    'n': 'o',
+    'l': 'p',
+    'm': 'q',
+    'f': 'r',
+    'o': 's',
+    'i': 't',
+    'g': 'u',
+    'j': 'v',
+    't': 'w',
+    'd': 'x',
+    'r': 'y',
+    'c': 'z',
+    '3': '0',
+    '8': '1',
+    '4': '2',
+    '0': '3',
+    '2': '4',
+    '7': '5',
+    '5': '6',
+    '9': '7',
+    '1': '8',
+    '6': '9'
+ }
+
+    letter = ""
+
+    for letter in str:
+        print(ENCODING[letter], end = "")
+
+decode_string("hgiikf")
+
+#*********PART TWO **************#
+
+# struggling to use # as delimiter and print items
+#gave up on using delimiter, went for quick and dirty solution that returned an answer
+
 """An ingredient has an amount and a description.
 For example: an Ingredient could have "1 cup" as the amount and "butter" as the description."""
 class Ingredient():
@@ -56,11 +113,12 @@ class Ingredient():
         self.amount = amount
         self.description = description
 
+# line_from_recipe = "8 vgl#hgiikf"
 
-# def decode_string(str): #completed
-#     """Given a string named str, use the Caesar encoding above to return the decoded string."""
+# def decode_ingredient(line):
+# #     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
 #     # TODO: implement me
-    
+
 #     ENCODING = {
 #     'y': 'a',
 #     'h': 'b',
@@ -99,68 +157,27 @@ class Ingredient():
 #     '1': '8',
 #     '6': '9'
 #  }
+#     #empty str
+#     text = ""
+#     new_ingredient = ''
 
-#     letter = ""
+#     for c in text:
+#         new_ingredient = str("".join(text(ENCODING[c]) if c in ENCODING else c for c in text))
+#         return new_ingredient
 
-#     for letter in str:
-#         print(ENCODING[letter], end = "")
+#     print(decode_ingredient("8 vgl#hgiikf"))
 
-# decode_string("hgiikf")
+def decode_ingredient(text):
+    line = text
 
-# struggling to use # as delimiter and print items
-# def decode_ingredient(line):
-#     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
-    # TODO: implement me
+    for c in line:
     
-line = "8 vgl#hgiikf"
-ENCODING = {
-    'y': 'a',
-    'h': 'b',
-    'v': 'c',
-    'x': 'd',
-    'k': 'e',
-    'p': 'f',
-    'z': 'g',
-    's': 'h',
-    'a': 'i',
-    'b': 'j',
-    'e': 'k',
-    'w': 'l',
-    'u': 'm',
-    'q': 'n',
-    'n': 'o',
-    'l': 'p',
-    'm': 'q',
-    'f': 'r',
-    'o': 's',
-    'i': 't',
-    'g': 'u',
-    'j': 'v',
-    't': 'w',
-    'd': 'x',
-    'r': 'y',
-    'c': 'z',
-    '3': '0',
-    '8': '1',
-    '4': '2',
-    '0': '3',
-    '2': '4',
-    '7': '5',
-    '5': '6',
-    '9': '7',
-    '1': '8',
-    '6': '9'
- }
-    
-for c in line:
-    if line[c] in ENCODING: 
-        solution += ENCODING[c]
-    else: 
-        solution += line[c]
+        solution = "".join(str(ENCODING[c]) if c in ENCODING else c for c in line)
     print(solution)
-        
 
-# decode_ingredient("nyiukyw")
+decode_ingredient("\n8 vgl#zfyqgwyikx ogzyf")
+
+#*********PART THREE************#
 
 def main(recipe): #completed
 #     """A program that decodes a secret recipe"""
@@ -207,7 +224,7 @@ def main(recipe): #completed
     
 
     #create new solution file
-    with open("decoded_recipe.txt", "w") as decoded_recipe: 
+    with open("/Users/carolina.scalzo/de-hours-with-experts/python/decoded_recipe.txt", "w") as decoded_recipe: 
         #parse source file and return decoded text
         for line in nonsense_recipe: 
 
