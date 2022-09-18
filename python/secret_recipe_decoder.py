b131b60d869d4df5c3c7caa235d84581acdf1651
aak7t3 <abdulkhan4786@gmail.com>
2022-09-18 13:09:03 -0500

    Implement challenge 3

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -3,6 +3,7 @@ import pydoc
 import sys
 import os
 
+
 # Caesar encoding, for use with decoding below
 ENCODING = {
     'y': 'a',
@@ -53,17 +54,59 @@ class Ingredient():
 
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
+<<<<<<< Updated upstream
     # TODO: implement me
+=======
+    #instantiate decoded string to return. 
+    decodedstr = ""
+    for letter in str:
+        if letter.isalnum() == False:
+            decoded = letter
+        #elif letter.isalnum == False
+        else:
+            decoded = ENCODING[letter]
+        decodedstr += decoded
+    return decodedstr
+>>>>>>> Stashed changes
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
+<<<<<<< Updated upstream
     # TODO: implement me
+=======
+    #create list to store decoded ingrediant parts
+    decodedList = []
+    inlist = line.split("#")
+    for part in inlist:
+        decodedList.append(decode_string(part))
+    
+
+    return Ingredient(decodedList[0], decodedList[1])
+>>>>>>> Stashed changes
 
 
 def main():
     """A program that decodes a secret recipe"""
-    # TODO: implement me
+    secretRecipe = open("secret_recipe.txt", "r")
+    decodedrecipe =  open("decoded_recipe.txt", "w+")
+    #Populate array with file text
+    recList = secretRecipe.read().split("\n")
+    #String to write to decoded text file. 
+    stringtoWrite=""
+    #Remove Blank Rows from array to keep decode_ingredient from erroring out.
+    recList = [x for x in recList if x] 
+    #iteratively decode and write to string
+    for line in recList:
+        #print(line)
+        recipeIngredient = decode_ingredient(line) #method returns Ingredient Object
+        stringtoWrite = stringtoWrite + recipeIngredient.amount + " " + recipeIngredient.description + "\n" 
+
+    #write string to decoded_recipe.txt
+    decodedrecipe.write(stringtoWrite)
+    decodedrecipe.close()
+
+    
 
 if __name__ == "__main__":
     main()
