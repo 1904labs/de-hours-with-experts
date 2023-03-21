d69b0ef7dbb48d9bbfbcdaa6d9c3a086240cfc27
Enrique Sampson <enrique@myadvocado.com>
2023-03-21 16:32:43 -0500

    finished version
    Not a scala person but tried my hand at this language. From what I understand I suppose I compile and run the file for every change? Very fun though, ill have to try this cookie recipe.

--- scala/src/main/scala/com/labs1904/SecretRecipeDecoder.scala
+++ scala/src/main/scala/com/labs1904/SecretRecipeDecoder.scala
@@ -1,6 +1,10 @@
 package com.labs1904
 
 import scala.collection.immutable.HashMap
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+
 
 /**
  * An ingredient has an amount and a description.
@@ -52,11 +56,10 @@ object SecretRecipeDecoder {
   /**
    * Given a string named str, use the Caeser encoding above to return the decoded string.
    * @param str A caesar-encoded string.
-   * @return
+   * @return The decoded string.
    */
   def decodeString(str: String): String = {
-    // todo: implement me
-    "1 cup"
+    str.map(c => ENCODING.getOrElse(c.toLower.toString, c.toString)).mkString("")
   }
 
   /**
@@ -65,8 +68,32 @@ object SecretRecipeDecoder {
    * @return
    */
   def decodeIngredient(line: String): Ingredient = {
-    // todo: implement me
-    Ingredient("1 cup", "butter")
+    val (amount, description) = line.split("#") match {
+      case Array(amount, desc) => (decodeString(amount), decodeString(desc))
+      case _ => ("", "")
+    }
+    Ingredient(amount, description)
+  }
+
+  /**
+   * Helper function to read files
+   * @param inputFileName
+   * @return
+   */
+  def readEncodedRecipe(inputFileName: String): List[String] = {
+    scala.io.Source.fromFile(inputFileName).getLines().toList
+  }
+
+  /**
+   * helper function to write decoded files
+   * @param outputFileName
+   * @param decodedRecipe
+   */
+  def writeDecodedRecipe(outputFileName: String, decodedRecipe: String): Unit = {
+    val file = new File(outputFileName)
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(decodedRecipe)
+    bw.close()
   }
 
   /**
@@ -74,6 +101,21 @@ object SecretRecipeDecoder {
    * @param args
    */
   def main(args: Array[String]): Unit = {
-    // TODO: implement me
+//    I could've done the read and write here but figured seperation of concerns wouldn't hurt.
+
+//    I'm not a scala person so ended up having to use absolute path instead of relative, tried to fix but wasn't able to.
+    val inputFileName = "/Users/enrique/Documents/GitHub/de-hours-with-experts/scala/src/main/resources/secret_recipe.txt"
+    val outputFileName = "/Users/enrique/Documents/GitHub/de-hours-with-experts/scala/src/main/resources/decoded_recipe.txt"
+
+//    read recipe
+    val encodedLines = readEncodedRecipe(inputFileName)
+//    decode the recipe
+    val decodedIngredients = encodedLines.map(decodeIngredient)
+//    seperate out the amount and description, ideally wouldve been done in decodeRecipe function but following the prompt
+    val decodedRecipe = decodedIngredients.map(i => s"${i.amount} ${i.description}").mkString("\n")
+//    write the recipe
+    writeDecodedRecipe(outputFileName, decodedRecipe)
+
+    println("The cookie recipe! :) " + outputFileName)
   }
 }
