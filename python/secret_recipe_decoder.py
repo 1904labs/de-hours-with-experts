0ddc08fdcf70752cf9c12034e6a74f553b7ebebf
Dalston Ward <ward.dalston@gmail.com>
2023-03-22 20:47:51 -0500

    Implement main
    Adds a __repr__ to Ingredients to make the output nicer.
    
    An alternative (if we can't change Ingredients) is a function like "def ingredients_str()"

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -49,6 +49,9 @@ class Ingredient():
     def __init__(self, amount, description) -> None:
         self.amount = amount
         self.description = description
+      
+    def __repr__(self) -> str:
+        return f'{self.amount} {self.description}'
 
 
 def decode_string(S: str) -> str:
@@ -63,7 +66,10 @@ def decode_ingredient(line: str) -> Ingredient:
 
 def main():
     """A program that decodes a secret recipe"""
-    # TODO: implement me
+    with open("secret_recipe.txt") as encoded_recipe, open("decoded_recipe.txt", "w") as decoded_recipe:
+      for line in encoded_recipe:
+        line_end = '\n' if line[-1] == '\n' else ''
+        decoded_recipe.write(f'{decode_ingredient(line.rstrip())}{line_end}')
 
 if __name__ == "__main__":
     main()
