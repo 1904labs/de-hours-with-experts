7e110d3679e5af8a49ddaa2e14675165b6624d3e
Samuel Ha <samqvha@gmail.com>
2023-03-24 14:06:47 -0500

    Upload decoder and decoded recipe

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -53,19 +53,62 @@ class Ingredient():
 
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
-    # TODO: implement me
-    return '1 cup'
+    
+    # Create new list variable that will hold the decoded characters
+    decoded_list = []
+    
+    # Loop through each character
+    for char in str:
+        # If char in ENCODING, then decode
+        if char in ENCODING:
+            # For each character in str, decode with the key-value pair in Caesar encoding
+            # Append decoded character to string
+            decoded_list.append(ENCODING.get(char))
+        else:
+            # If not in ENCODING, then simply include char in string
+            decoded_list.append(char)
+    
+    # Join all characters together
+    decoded_str = ''.join(decoded_list)
+    return decoded_str
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
-    # TODO: implement me
-    return Ingredient("1 cup", "butter")
+
+    # Split line into strings
+    strings = line.split('#')
+    
+    # Decode first string
+    amount = decode_string(strings[0])    
+    
+    # Decode second string
+    description = decode_string(strings[1])
+    new_Ingredient = Ingredient(amount, description)
+    return new_Ingredient
 
 
 def main():
     """A program that decodes a secret recipe"""
-    # TODO: implement me
+    
+    # Create list to hold lines of recipe
+    recipe = []
+
+    # Open up recipe text file
+    with open("secret_recipe.txt") as fp:
+        Lines = fp.readlines()
+        for line in Lines:
+            recipe.append(decode_ingredient(line))
+            
+    # Open up decoded recipe text file to write into
+    with open('decoded_recipe.txt', 'w') as f:
+        
+        # Loop through each line of a recipe
+        for line in recipe:
+            # Place both ingredient and description into decoded recipe text file
+            f.write(line.amount)
+            f.write(' ')
+            f.write(line.description)
 
 if __name__ == "__main__":
     main()
