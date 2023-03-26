4c209084ab8c55a043359341cfbf3844e74904a0
Christopher Wike <chriswike@gmail.com>
2023-03-25 21:57:27 -0500

    Finished Challenge Three

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -41,7 +41,7 @@ ENCODING = {
     '9': '7',
     '1': '8',
     '6': '9'
- }
+}
 
 """An ingredient has an amount and a description.
 For example: an Ingredient could have "1 cup" as the amount and "butter" as the description."""
@@ -54,19 +54,41 @@ class Ingredient():
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
-    #
-    return '1 cup'
+    str_len = len(str)
+    plain_text_list = [None] * str_len
+
+    for i in range(str_len):
+        if str[i].isalnum():
+            plain_text_list[i] = ENCODING[str[i].lower()]
+        else:
+            plain_text_list[i] = str[i]
+
+    return ''.join(plain_text_list)
+    # return '1 cup'
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
-    return Ingredient("1 cup", "butter")
+    line_as_list = line.split("#")
+
+    description = decode_string(line_as_list[0])
+    amount = decode_string(line_as_list[1])
+
+    return Ingredient(description, amount)
+    # return Ingredient("1 cup", "butter")
 
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+    with open("secret_recipe.txt") as cipher_file:
+        lines = cipher_file.readlines()
+        with open("decoded_recipe.txt", "a") as plain_file:
+            for line in lines:
+                ingredient = decode_ingredient(line)
+                plain_file.write(ingredient.amount + ingredient.description)
+
 
 if __name__ == "__main__":
     main()
