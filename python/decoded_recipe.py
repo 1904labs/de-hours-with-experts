214ef65bc66dc56e107d7ee117c040b7e99d99c8
Mandypkl <100866097+Mandypkl@users.noreply.github.com>
2023-03-27 15:50:22 -0500

    create a decoded recipe

---
+++ python/decoded_recipe.py
@@ -0,0 +1,25 @@
+amount: 1 cup, item: butter
+
+amount: 1 cup, item: dark brown sugar, packed
+
+amount: 1 cup, item: granulated sugar
+
+amount: 2, item: eggs
+
+amount: 1 teaspoon, item: vanilla
+
+amount: 2 1/2 cups, item: oatmeal
+
+amount: 2 cups, item: flour
+
+amount: 1/2 teaspoon, item: salt
+
+amount: 1 teaspoon, item: baking soda
+
+amount: 1 teaspoon, item: baking powder
+
+amount: 12 ounces, item: chocolate chips
+
+amount: 1, item: 4-ounce milk chocolate bar
+
+amount: 1 1/2 cups, item: chopped nuts
