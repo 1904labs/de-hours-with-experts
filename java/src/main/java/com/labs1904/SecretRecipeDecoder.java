ca2767a954adab194a5af01b8f92cfa3887723c2
Nag N <nn7949@gmail.com>
2023-03-27 20:04:51 -0500

    initial commit

--- java/src/main/java/com/labs1904/SecretRecipeDecoder.java
+++ java/src/main/java/com/labs1904/SecretRecipeDecoder.java
@@ -1,8 +1,15 @@
 package com.labs1904;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class SecretRecipeDecoder {
     private static Map<String, String> ENCODING = new HashMap<String, String>() {
@@ -52,8 +59,22 @@ public class SecretRecipeDecoder {
      * @return
      */
     public static String decodeString(String str) {
+        StringBuilder result = new StringBuilder(str.length());
+        String[] splitString = str.split("");
+
+        //iterate char array
+        for (int i = 0; i < splitString.length; i++) {
+            //System.out.print(splitString[i] + " ");
+            String decodedString = ENCODING.get(splitString[i]);
+            if(decodedString == null)
+                result.append(splitString[i]);
+            else result.append(ENCODING.get(splitString[i]));
+        }
+
+        //System.out.print(result.toString());
+
         // TODO: implement me
-        return "1 cup";
+        return result.toString();
     }
 
     /**
@@ -62,11 +83,54 @@ public class SecretRecipeDecoder {
      * @return
      */
     public static Ingredient decodeIngredient(String line) {
-        // TODO: implement me
-        return new Ingredient("1 cup", "butter");
+        String[] splitString = line.split("#| ");
+        Ingredient theIngred = new Ingredient("","");
+
+        for (int i = 0; i < splitString.length; i++) {
+            if(i == 0)
+                theIngred.setAmount(decodeString(splitString[i]));
+            else if(i == 1) {
+                //System.out.println(splitString[i]);
+                theIngred.setDescription(decodeString(splitString[i]));
+            }
+            else if(i == 2){
+                theIngred.setDescription(theIngred.getDescription() + " " + decodeString(splitString[i]));
+            }
+        }
+
+        return theIngred;
     }
 
-    public static void main(String[] args) {
+    public static void DecodeRecipe(String filepath) throws FileNotFoundException, URISyntaxException {
+        Scanner scanner = new Scanner(new File(filepath));
+
+        URL res = SecretRecipeDecoder.class.getClassLoader().getResource("decoded_recipe.txt");
+        File file = Paths.get(res.toURI()).toFile();
+        String destinationPath = file.getAbsolutePath();
+        System.out.println(destinationPath);
+        PrintWriter writer = new PrintWriter(file);
+
+        Ingredient t = null;
+        while (scanner.hasNextLine()) {
+            //System.out.println();
+            String txtLine = scanner.nextLine();
+            //System.out.println(txtLine);
+            t = decodeIngredient(txtLine);
+            writer.println(t.getAmount() + " " + t.getDescription());
+            System.out.println(t.getAmount() + " " + t.getDescription());
+        }
+
+        scanner.close();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
         // TODO: implement me
+        SecretRecipeDecoder.decodeString("hgiikf");
+        Ingredient result = SecretRecipeDecoder.decodeIngredient("8 vgl#hgiikf");
+        URL res = SecretRecipeDecoder.class.getClassLoader().getResource("secret_recipe.txt");
+        File file = Paths.get(res.toURI()).toFile();
+        String absolutePath = file.getAbsolutePath();
+        DecodeRecipe(absolutePath);
     }
 }
