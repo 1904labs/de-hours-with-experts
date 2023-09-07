56c00acf48998aa65ae4c7aa388bf6caeed88d1a
paul <paulentz@gmail.com>
2023-09-07 17:53:32 -0500

    done with part 2 of challenge 3

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -5,47 +5,49 @@ import os
 
 # Caesar encoding, for use with decoding below
 ENCODING = {
-    'y': 'a',
-    'h': 'b',
-    'v': 'c',
-    'x': 'd',
-    'k': 'e',
-    'p': 'f',
-    'z': 'g',
-    's': 'h',
-    'a': 'i',
-    'b': 'j',
-    'e': 'k',
-    'w': 'l',
-    'u': 'm',
-    'q': 'n',
-    'n': 'o',
-    'l': 'p',
-    'm': 'q',
-    'f': 'r',
-    'o': 's',
-    'i': 't',
-    'g': 'u',
-    'j': 'v',
-    't': 'w',
-    'd': 'x',
-    'r': 'y',
-    'c': 'z',
-    '3': '0',
-    '8': '1',
-    '4': '2',
-    '0': '3',
-    '2': '4',
-    '7': '5',
-    '5': '6',
-    '9': '7',
-    '1': '8',
-    '6': '9'
- }
+    "y": "a",
+    "h": "b",
+    "v": "c",
+    "x": "d",
+    "k": "e",
+    "p": "f",
+    "z": "g",
+    "s": "h",
+    "a": "i",
+    "b": "j",
+    "e": "k",
+    "w": "l",
+    "u": "m",
+    "q": "n",
+    "n": "o",
+    "l": "p",
+    "m": "q",
+    "f": "r",
+    "o": "s",
+    "i": "t",
+    "g": "u",
+    "j": "v",
+    "t": "w",
+    "d": "x",
+    "r": "y",
+    "c": "z",
+    "3": "0",
+    "8": "1",
+    "4": "2",
+    "0": "3",
+    "2": "4",
+    "7": "5",
+    "5": "6",
+    "9": "7",
+    "1": "8",
+    "6": "9",
+}
 
 """An ingredient has an amount and a description.
 For example: an Ingredient could have "1 cup" as the amount and "butter" as the description."""
-class Ingredient():
+
+
+class Ingredient:
     def __init__(self, amount, description) -> None:
         self.amount = amount
         self.description = description
@@ -54,18 +56,35 @@ class Ingredient():
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
-    return '1 cup'
+    newStr = ""
+    for x in str:
+        if x == " ":
+            newStr += x
+        for key in ENCODING:
+            if key == x:
+                newStr += ENCODING[x]
+    return newStr
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
-    return Ingredient("1 cup", "butter")
+    newLine = line.split("#")
+    newArray = []
+    for x in newLine:
+        newArray.append(decode_string(x))
+    newIngredient = Ingredient(newArray[0], newArray[1])
+    return newIngredient
 
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
 
+
 if __name__ == "__main__":
     main()
+
+
+print(decode_string("8 vgl"))
+print(decode_ingredient("8 vgl#hgiikf"))
