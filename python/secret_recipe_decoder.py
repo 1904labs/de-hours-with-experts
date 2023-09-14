a13b745cd3eb4d452031460e06e742412130b971
Conway <kolmercm@gmail.com>
2023-09-13 21:02:36 -0500

    first commit

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -50,22 +50,58 @@ class Ingredient():
         self.amount = amount
         self.description = description
 
+    def toStr(self):
+         return self.amount + " " + self.description
+
 
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
-    return '1 cup'
+    decoded = ""
+    for ch in str:
+        if ch in ENCODING:
+             decoded = decoded + ENCODING[ch]
+             continue
+        if ch == "\n":
+                continue 
+        decoded = decoded + ch
+        # add ENCODiNG[ch] to decoded
+    print(decoded)
+    return decoded
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
-    return Ingredient("1 cup", "butter")
+    splitline = line.split('#')
+    print(splitline)
+    amount = decode_string(splitline[0])
+    description =decode_string(splitline[1])
+    return Ingredient(amount, description)
 
 
 def main():
     """A program that decodes a secret recipe"""
-    # TODO: implement me
+    # decode_string('hgiikf')
+    # decode_ingredient('8 vgl#hgiikf')
+
+    # Open files
+    inputfile = open("python/secret_recipe.txt", "r")
+    outputfile = open("decoded_recipe.txt", "w")
+
+    # For each line in the input file
+    for line in inputfile:
+        print("input: " + line)
+        # get the decoded ingredient using our function from step 2
+        ingredient = decode_ingredient(line)
+        # write the decoded ingredient to a new line in the output file
+        outputfile.write(ingredient.toStr() + "\n")
+        print("output" + ingredient.toStr())
+
+    # Close files
+    inputfile.close()
+    outputfile.close()
 
 if __name__ == "__main__":
     main()
+
