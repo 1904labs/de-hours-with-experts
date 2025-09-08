f752e332040b03ed91fa2dd97ae774f45a7a5fe7
Jeffery Phipps <jefferyphipps@att.net>
2025-09-08 16:45:05 -0500

    solved puzzle

--- python/secret_recipe_decoder.py
+++ python/secret_recipe_decoder.py
@@ -54,18 +54,39 @@ class Ingredient():
 def decode_string(str):
     """Given a string named str, use the Caesar encoding above to return the decoded string."""
     # TODO: implement me
-    return '1 cup'
+    answer = ''
+    for letter in str:
+        if letter not in ENCODING:
+            answer += letter
+        else:
+            answer += ENCODING[letter]
+    return answer
 
 
 def decode_ingredient(line):
     """Given an ingredient, decode the amount and description, and return a new Ingredient"""
     # TODO: implement me
-    return Ingredient("1 cup", "butter")
+    #print(line)
+    amount = decode_string(line.split('#')[0])
+    description = decode_string(line.split('#')[1])
+    return Ingredient(amount, description)
 
 
 def main():
     """A program that decodes a secret recipe"""
     # TODO: implement me
+    recipe = ''
+    with open('secret_recipe.txt', 'r') as file:
+        for line in file:
+            ingredient = decode_ingredient(line)
+            print(ingredient.amount, ingredient.description)
+            answer_line = ingredient.amount + ' ' + ingredient.description +"\n"
+            recipe += answer_line
+        with open("decoded_recipe.txt", "w") as answer:
+            answer.write(recipe)
+
+
+
 
 if __name__ == "__main__":
     main()
